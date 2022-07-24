b85ee4634967834070ed7415f399fe443bf54490
RobinTTY <muellerobin95@gmail.com>
2022-07-24 21:37:38 +0200

    Add endpoint urls

---
+++ src/RobinTTY.NordigenApiClient/Endpoints/NordigenEndpointUrls.cs
@@ -0,0 +1,9 @@
+﻿namespace RobinTTY.NordigenApiClient.Endpoints;
+internal class NordigenEndpointUrls
+{
+    private const string Base = "https://ob.nordigen.com/api/v2/";
+    internal static readonly string TokensEndpoint = Base + "token/";
+    internal static readonly string AccountsEndpoint = Base + "agreements/enduser/";
+    internal static readonly string InstitutionsEndpoint = Base + "insitutions/";
+    internal static readonly string RequisitionsEndpoint = Base + "requisitions/";
+}
