3f8a3dc78cde30ec88a2a0ac056f9113feda984e
RobinTTY <muellerobin95@gmail.com>
2022-08-05 05:38:02 +0200

    Add requisition model

---
+++ src/RobinTTY.NordigenApiClient/Models/Requests/CreateRequisitionRequest.cs
@@ -0,0 +1,45 @@
+﻿namespace RobinTTY.NordigenApiClient.Models.Requests;
+
+/// <summary>
+/// The parts of a requisition that are necessary to create it via the Nordigen API.
+/// </summary>
+public class CreateRequisitionRequest
+{
+    /// <summary>
+    /// URI where the end user will be redirected after finishing authentication.
+    /// </summary>
+    public Uri Redirect { get; }
+    /// <summary>
+    /// The id of the institution this requisition is linked to.
+    /// </summary>
+    public string InstitutionId { get; }
+    /// <summary>
+    /// The agreement this requisition is linked to.
+    /// </summary>
+    public Guid AgreementId { get; }
+    /// <summary>
+    /// A unique ID set by the user of the API for internal referencing.
+    /// </summary>
+    public string Reference { get; }
+    /// <summary>
+    /// Enforces a language for all end user steps hosted by Nordigen passed as a two-letter country code <see href="https://wikipedia.org/wiki/ISO_639-1">(ISO 639-1)</see>.
+    /// </summary>
+    public string UserLanguage { get; }
+    /// <summary>
+    /// Some European banks allow sending an end-user's SSN to check whether the SSN is valid.
+    /// <para>For bank availability check: <see href="https://nordigen.com/en/blog/new-feature-ssn-verification-using-open-banking/"/>.</para>
+    /// </summary>
+    public string SocialSecurityNumber { get; }
+    /// <summary>
+    /// Enables the end user to select which accounts they want to share (like joint accounts, accounts of children, etc.) if set to true.
+    /// <para>For details see: <see href="https://ob.helpscoutdocs.com/article/142-account-selection-feature"/>.</para>
+    /// </summary>
+    public bool AccountSelection { get; }
+    /// <summary>
+    /// Enables you to redirect end users back to your app immediately after they have given their consent to access the account information data from the bank,
+    /// instead of waiting for transaction data being processed. Accounts endpoint status will be PROCESSING and you have to wait until account status is READY
+    /// before you're able to query the transactions.
+    /// <para>For details see: <see href="https://ob.helpscoutdocs.com/article/145-immediate-end-user-redirect-from-bank-after-consent"/>.</para>
+    /// </summary>
+    public bool RedirectImmediate { get; }
+}
---
+++ src/RobinTTY.NordigenApiClient/Models/Responses/Requisition.cs
@@ -0,0 +1,49 @@
+﻿using RobinTTY.NordigenApiClient.Models.Requests;
+
+namespace RobinTTY.NordigenApiClient.Models.Responses;
+
+/// <summary>
+/// A collection of inputs for creating links and retrieving accounts via the Nordigen API.
+/// </summary>
+public class Requisition : CreateRequisitionRequest
+{
+    /// <summary>
+    /// The id of the requisition assigned by the Nordigen API.
+    /// </summary>
+    public Guid Id { get; }
+    /// <summary>
+    /// The time this requisition was created.
+    /// </summary>
+    public DateTime Created { get; }
+    /// <summary>
+    /// The status of the requisition.
+    /// </summary>
+    public RequisitionStatus Status { get; }
+    /// <summary>
+    /// The accounts linked to this requisition.
+    /// </summary>
+    public List<Guid> Accounts { get; }
+    /// <summary>
+    /// The Uri which starts the authentication process.
+    /// </summary>
+    public Uri AuthenticationLink { get; }
+}
+
+/// <summary>
+/// The status of the requisition.
+/// </summary>
+public class RequisitionStatus
+{
+    /// <summary>
+    /// Abbreviation of the requisition status.
+    /// </summary>
+    public string Short { get; }
+    /// <summary>
+    /// The requisition status.
+    /// </summary>
+    public string Long { get; }
+    /// <summary>
+    /// Description of the requisition status.
+    /// </summary>
+    public string Description { get; }
+}
