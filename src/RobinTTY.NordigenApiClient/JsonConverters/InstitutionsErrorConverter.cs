84a156473687e6bfda6b4206f31dcff9c9151d07
RobinTTY <muellerobin95@gmail.com>
2023-09-08 15:05:51 +0200

    Bump dependencies

--- src/RobinTTY.NordigenApiClient/JsonConverters/InstitutionsErrorConverter.cs
+++ src/RobinTTY.NordigenApiClient/JsonConverters/InstitutionsErrorConverter.cs
@@ -11,7 +11,7 @@ internal class InstitutionsErrorConverter : JsonConverter<InstitutionsError>
         var error = JsonSerializer.Deserialize<InstitutionsErrorInternal>(ref reader, options);
         if (error is not null)
             return error.Country is not null ? new InstitutionsError(error.Country) : new InstitutionsError(new BasicError(error.Summary!, error.Detail!));
-        throw new SerializationException("Couldn't deserialize institutions error. please report this issue to the library author: https://github.com/RobinTTY/NordigenApiClient/issues.");
+        throw new SerializationException("Couldn't deserialize institutions error, please report this issue to the library author: https://github.com/RobinTTY/NordigenApiClient/issues.");
     }
 
     public override void Write(Utf8JsonWriter writer, InstitutionsError value, JsonSerializerOptions options)
