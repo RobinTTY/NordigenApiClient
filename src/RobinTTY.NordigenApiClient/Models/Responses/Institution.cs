62301f1cb1cd376e80e93ad9d7b41a6c21d3ff0d
RobinTTY <muellerobin95@gmail.com>
2024-04-16 01:25:59 +0200

    Add new fields to Institution class

--- src/RobinTTY.NordigenApiClient/Models/Responses/Institution.cs
+++ src/RobinTTY.NordigenApiClient/Models/Responses/Institution.cs
@@ -1,4 +1,5 @@
 ﻿using System.Text.Json.Serialization;
+using RobinTTY.NordigenApiClient.Endpoints;
 
 namespace RobinTTY.NordigenApiClient.Models.Responses;
 
@@ -43,6 +44,24 @@ public class Institution
     /// </summary>
     [JsonPropertyName("logo")]
     public Uri Logo { get; }
+    
+    /// <summary>
+    /// Supported payment products for this institution. Only populated when calling <see cref="InstitutionsEndpoint.GetInstitution"/>.
+    /// </summary>
+    [JsonPropertyName("supported_payments")]
+    public SupportedPayments? SupportedPayments { get; }
+    
+    /// <summary>
+    /// Supported features for this institution. Only populated when calling <see cref="InstitutionsEndpoint.GetInstitution"/>.
+    /// </summary>
+    [JsonPropertyName("supported_features")]
+    public List<string>? SupportedFeatures { get; }
+    
+    /// <summary>
+    /// Undocumented field returned by the GoCardless API. Only populated when calling <see cref="InstitutionsEndpoint.GetInstitution"/>.
+    /// </summary>
+    [JsonPropertyName("identification_codes")]
+    public List<string>? IdentificationCodes { get; }
 
     /// <summary>
     /// Creates a new instance of <see cref="Institution" />.
@@ -53,9 +72,12 @@ public class Institution
     /// <param name="transactionTotalDays">The days for which the transaction history is available.</param>
     /// <param name="countries">The countries the institution operates in.</param>
     /// <param name="logo">A <see cref="Uri" /> for the logo of the institution.</param>
+    /// <param name="supportedPayments">Supported payment products for this institution.</param>
+    /// <param name="supportedFeatures">Supported features for this institution.</param>
+    /// <param name="identificationCodes">Undocumented field returned by the GoCardless API.</param>
     [JsonConstructor]
     public Institution(string id, string name, string bic, uint transactionTotalDays, List<string> countries,
-        Uri logo)
+        Uri logo, SupportedPayments? supportedPayments, List<string>? supportedFeatures, List<string>? identificationCodes)
     {
         Id = id;
         Name = name;
@@ -63,5 +85,29 @@ public class Institution
         TransactionTotalDays = transactionTotalDays;
         Countries = countries;
         Logo = logo;
+        SupportedPayments = supportedPayments;
+        SupportedFeatures = supportedFeatures;
+        IdentificationCodes = identificationCodes;
+    }
+}
+
+/// <summary>
+/// The payment products supported by an institution.
+/// </summary>
+public class SupportedPayments
+{
+    /// <summary>
+    /// Supported payment products in the single-payment category.
+    /// </summary>
+    [JsonPropertyName("single-payment")]
+    public List<PaymentProduct> SinglePayment { get; }
+    
+    /// <summary>
+    /// Creates a new instance of <see cref="SupportedPayments"/>
+    /// </summary>
+    /// <param name="singlePayment">Supported payment products in the single-payment category.</param>
+    public SupportedPayments(List<PaymentProduct> singlePayment)
+    {
+        SinglePayment = singlePayment;
     }
 }
---
+++ src/RobinTTY.NordigenApiClient/Models/Responses/PaymentProduct.cs
@@ -0,0 +1,89 @@
+﻿using System.ComponentModel;
+using System.Text.Json.Serialization;
+using RobinTTY.NordigenApiClient.JsonConverters;
+
+namespace RobinTTY.NordigenApiClient.Models.Responses;
+
+/// <summary>
+/// A payment product offered by an institution.
+/// </summary>
+[JsonConverter(typeof(EnumDescriptionConverter<PaymentProduct>))]
+public enum PaymentProduct
+{
+    /// <summary>
+    /// An undefined payment product type. Assigned if the type couldn't be matched to any known types.
+    /// </summary>
+    Undefined,
+
+    /// <summary>
+    /// TARGET2 is the real-time gross settlement (RTGS) system owned and operated by the Eurosystem.
+    /// </summary>
+    [Description("T2P")]
+    Target2Payments,
+
+    /// <summary>
+    /// SEPA Credit Transfer, more commonly abbreviated as SCT, is a payment processing scheme used for making one-time, euro-denominated fund transfers between banks and payment service providers (PSPs) in the SEPA area.
+    /// </summary>
+    [Description("SCT")]
+    SepaCreditTransfers,
+
+    /// <summary>
+    /// Instant SEPA Credit Transfer supports money transfers of up to €100,000 between participating banks or PSPs in the SEPA area in real or near-real time.
+    /// </summary>
+    [Description("ISCT")]
+    InstantSepaCreditTransfer,
+
+    /// <summary>
+    /// Cross-border payments are transactions sent from one country and received in a different country.
+    /// </summary>
+    [Description("CBCT")]
+    CrossBorderCreditTransfers,
+
+    /// <summary>
+    /// Bacs Payment Schemes Limited (Bacs), previously known as Bankers' Automated Clearing System, is responsible for the clearing and settlement of UK automated direct debit and Bacs Direct Credit and the provision of third-party services.
+    /// </summary>
+    [Description("BACS")]
+    BacsPaymentSchemesLimited,
+
+    /// <summary>
+    /// The Clearing House Automated Payment System (CHAPS) is a real-time gross settlement payment system used for sterling transactions in the United Kingdom.
+    /// </summary>
+    [Description("CHAPS")]
+    ClearingHouseAutomatedPaymentSystem,
+
+    /// <summary>
+    /// The Faster Payments Service (FPS) is a United Kingdom banking initiative to reduce payment times between different banks' customer accounts to typically a few seconds, from the three working days that transfers usually take using the long-established BACS system.
+    /// </summary>
+    [Description("FPS")]
+    FasterPaymentScheme,
+
+    /// <summary>
+    /// SWIFT payments are international electronic transactions facilitated by an intermediary bank.
+    /// </summary>
+    [Description("SWIFT")]
+    SwiftPaymentService,
+
+    /// <summary>
+    /// A balance transfer is the transfer of the balance in an account to another account, often held at another institution. It is most commonly used when describing a credit card balance transfer.
+    /// </summary>
+    [Description("BT")]
+    BalanceTransfer,
+
+    /// <summary>
+    /// Money transfer generally refers to one of several cashless modes of payment or payment systems.
+    /// </summary>
+    [Description("MT")]
+    MoneyTransfer,
+
+    /// <summary>
+    /// A domestic transfer is a transfer completed between accounts that are held in the same country.
+    /// </summary>
+    [Description("DCT")]
+    DomesticCreditTransfer,
+
+    /// <summary>
+    /// An instant domestic transfer is a transfer completed between accounts that are held in the same country in real-time.
+    /// </summary>
+    [Description("IDCT")]
+    InstantDomesticCreditTransfer
+}
