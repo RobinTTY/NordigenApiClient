069aa40f828524be83f387e28e064835e537686e
RobinTTY <muellerobin95@gmail.com>
2024-04-21 02:21:58 +0200

    Refactor authentication related tests

--- src/RobinTTY.NordigenApiClient.Tests/LiveApi/CredentialTests.cs
+++ src/RobinTTY.NordigenApiClient.Tests/LiveApi/CredentialTests.cs
@@ -1,80 +1,53 @@
 ﻿using System.Net;
 using RobinTTY.NordigenApiClient.Models;
-using RobinTTY.NordigenApiClient.Models.Requests;
-using RobinTTY.NordigenApiClient.Models.Responses;
 using RobinTTY.NordigenApiClient.Tests.Shared;
 
 namespace RobinTTY.NordigenApiClient.Tests.LiveApi;
 
-/// <summary>
-/// Tests the instantiation of the <see cref="NordigenClient" />.
-/// </summary>
-internal class CredentialTests
+public class CredentialTests
 {
-    private readonly string[] _secrets = File.ReadAllLines("secrets.txt");
+    private NordigenClient _apiClient = null!;
 
+    [OneTimeSetUp]
+    public void Setup()
+    {
+        _apiClient = TestHelpers.GetConfiguredClient();
+    }
+    
     /// <summary>
-    /// Tests the creation of the <see cref="NordigenClient" /> with invalid credentials.
-    /// The credentials have the correct structure but were not issued for use.
+    /// Tests the failure of authentication when trying to execute a request using the live API.
     /// </summary>
     [Test]
-    public async Task MakeRequestWithInvalidCredentials()
+    public async Task ExecuteRequestWithInvalidCredentials()
     {
         using var httpClient = new HttpClient();
         var invalidCredentials = new NordigenClientCredentials("01234567-89ab-cdef-0123-456789abcdef",
             "0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef");
         var apiClient = new NordigenClient(httpClient, invalidCredentials);
 
-        // Returns AccountsError
-        var balancesResponse = await apiClient.AccountsEndpoint.GetBalances(_secrets[9]);
+        // Returns BasicError
+        var agreementsResponse = await apiClient.TokenEndpoint.GetTokenPair();
         Assert.Multiple(() =>
         {
-            Assert.That(balancesResponse.StatusCode, Is.EqualTo(HttpStatusCode.Unauthorized));
-            AssertErrorMatchesExpectation(balancesResponse.Error!);
-            Assert.That(
-                new object?[]
-                {
-                    balancesResponse.Error!.EndDateError, balancesResponse.Error.StartDateError,
-                    balancesResponse.Error.Type
-                }, Has.All.Null);
-        });
-
-        // Returns CreateAgreementError
-        var agreementRequest = new CreateAgreementRequest(90, 90,
-            ["balances", "details", "transactions"], "SANDBOXFINANCE_SFIN0000");
-        var createAgreementResponse = await apiClient.AgreementsEndpoint.CreateAgreement(agreementRequest);
-        Assert.Multiple(() =>
-        {
-            Assert.That(balancesResponse.StatusCode, Is.EqualTo(HttpStatusCode.Unauthorized));
-            AssertErrorMatchesExpectation(createAgreementResponse.Error!);
-            Assert.That(new object?[]
-            {
-                createAgreementResponse.Error!.AccessScopeError, createAgreementResponse.Error.AccessValidForDaysError,
-                createAgreementResponse.Error.AgreementError,
-                createAgreementResponse.Error.InstitutionIdError, createAgreementResponse.Error.MaxHistoricalDaysError
-            }, Has.All.Null);
-        });
-
-        // Returns CreateRequisitionError
-        const string institutionId = "SANDBOXFINANCE_SFIN0000";
-        var requisitionRequest =
-            new CreateRequisitionRequest(new Uri("https://robintty.com"), institutionId, "some_reference", "EN");
-        var requisitionResponse = await apiClient.RequisitionsEndpoint.CreateRequisition(requisitionRequest);
-        Assert.Multiple(() =>
-        {
-            Assert.That(balancesResponse.StatusCode, Is.EqualTo(HttpStatusCode.Unauthorized));
-            AssertErrorMatchesExpectation(requisitionResponse.Error!);
-            Assert.That(new object?[]
-            {
-                requisitionResponse.Error!.AgreementError, requisitionResponse.Error.InstitutionIdError,
-                requisitionResponse.Error.AccountSelectionError,
-                requisitionResponse.Error.RedirectError, requisitionResponse.Error.ReferenceError,
-                requisitionResponse.Error.SocialSecurityNumberError, requisitionResponse.Error.UserLanguageError
-            }, Has.All.Null);
+            AssertionHelpers.AssertNordigenApiResponseIsUnsuccessful(agreementsResponse, HttpStatusCode.Unauthorized);
+            AssertionHelpers.AssertBasicResponseMatchesExpectations(agreementsResponse.Error, "Authentication failed",
+                "No active account found with the given credentials");
         });
     }
-
-    private static void AssertErrorMatchesExpectation(BasicResponse error) =>
-        AssertionHelpers.AssertBasicResponseMatchesExpectations(error, "Authentication failed",
-            "No active account found with the given credentials");
+    
+    /// <summary>
+    /// Tests that <see cref="NordigenClient.JsonWebTokenPair" /> is populated after the first authenticated request is made.
+    /// </summary>
+    [Test]
+    public async Task CheckValidTokensAfterRequest()
+    {
+        Assert.That(_apiClient.JsonWebTokenPair, Is.Null);
+        await _apiClient.RequisitionsEndpoint.GetRequisitions(5, 0, CancellationToken.None);
+        Assert.Multiple(() =>
+        {
+            Assert.That(_apiClient.JsonWebTokenPair, Is.Not.Null);
+            Assert.That(_apiClient.JsonWebTokenPair!.AccessToken.EncodedToken, Has.Length.GreaterThan(0));
+            Assert.That(_apiClient.JsonWebTokenPair!.RefreshToken.EncodedToken, Has.Length.GreaterThan(0));
+        });
+    }
 }
---
+++ src/RobinTTY.NordigenApiClient.Tests/Mocks/AuthenticationTests.cs
@@ -0,0 +1,46 @@
+﻿using System.Net;
+using RobinTTY.NordigenApiClient.Tests.Shared;
+
+namespace RobinTTY.NordigenApiClient.Tests.Mocks;
+
+public class AuthenticationTests
+{
+    /// <summary>
+    /// Tests the failure of authentication when trying to execute a request using mocks.
+    /// </summary>
+    [Test]
+    public async Task GetTokenPairWithInvalidCredentials()
+    {
+        var apiClient = TestHelpers.GetMockClient(
+            TestHelpers.MockData.TokenEndpointMockData.NoAccountForGivenCredentialsError,
+            HttpStatusCode.Unauthorized, addDefaultAuthToken: false);
+
+        var tokenPairResponse = await apiClient.TokenEndpoint.GetTokenPair();
+
+        Assert.Multiple(() =>
+        {
+            AssertionHelpers.AssertNordigenApiResponseIsUnsuccessful(tokenPairResponse, HttpStatusCode.Unauthorized);
+            AssertionHelpers.AssertBasicResponseMatchesExpectations(tokenPairResponse.Error, "Authentication failed",
+                "No active account found with the given credentials");
+        });
+    }
+    
+    /// <summary>
+    /// Tests that <see cref="NordigenClient.JsonWebTokenPair" /> is populated after the first authenticated request is made.
+    /// </summary>
+    [Test]
+    public async Task CheckValidTokensAfterRequest()
+    {
+        var apiClient = TestHelpers.GetMockClient(TestHelpers.MockData.RequisitionsEndpointMockData.GetRequisitions, HttpStatusCode.OK);
+        Assert.That(apiClient.JsonWebTokenPair, Is.Null);
+        
+        await apiClient.RequisitionsEndpoint.GetRequisitions(5, 0, CancellationToken.None);
+        
+        Assert.Multiple(() =>
+        {
+            Assert.That(apiClient.JsonWebTokenPair, Is.Not.Null);
+            Assert.That(apiClient.JsonWebTokenPair!.AccessToken.EncodedToken, Has.Length.GreaterThan(0));
+            Assert.That(apiClient.JsonWebTokenPair!.RefreshToken.EncodedToken, Has.Length.GreaterThan(0));
+        });
+    }
+}
