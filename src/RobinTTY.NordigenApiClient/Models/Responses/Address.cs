8f6c6c8b13e32e3ab16672e588570ac01c6b98a5
RobinTTY <muellerobin95@gmail.com>
2024-05-14 03:12:33 +0200

    Add OwnerAddressStructured and DisplayName properties to BankAccountDetails

---
+++ src/RobinTTY.NordigenApiClient/Models/Responses/Address.cs
@@ -0,0 +1,39 @@
+﻿using System.Text.Json.Serialization;
+
+namespace RobinTTY.NordigenApiClient.Models.Responses;
+
+/// <summary>
+/// The particulars of the place where someone lives or an organization is situated.
+/// </summary>
+public class Address
+{
+    /// <summary>
+    /// The name of the street.
+    /// </summary>
+    [JsonPropertyName("streetName")]
+    public string? StreetName { get; }
+    
+    /// <summary>
+    /// The number of the building.
+    /// </summary>
+    [JsonPropertyName("buildingNumber")]
+    public string? BuildingNumber { get; }
+    
+    /// <summary>
+    /// The postcode of the town.
+    /// </summary>
+    [JsonPropertyName("postCode")]
+    public string? PostCode { get; }
+    
+    /// <summary>
+    /// The name of the town.
+    /// </summary>
+    [JsonPropertyName("townName")]
+    public string? TownName { get; }
+    
+    /// <summary>
+    /// The name of the country.
+    /// </summary>
+    [JsonPropertyName("country")]
+    public string? Country { get; }
+}
